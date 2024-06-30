a59d71c3af0d70f09ca1e8e28e3904d5ce3d17ec
Santy <sanpiquin@gmail.com>
2024-06-30 11:28:09 -0300

    .

--- src/app/books copy/edit-book/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import EditBookController from '@/Controllers/Books/EditBookController'
-
-export default function Page ({ params }: { params: { id: string } }) {
-  const id = params.id
-
-  return (
-    <EditBookController id={id} />
-  )
-}
