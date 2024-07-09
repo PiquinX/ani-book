f2c7800ee8233ef16677ab1269eccaeb7106bb02
Santy <sanpiquin@gmail.com>
2024-07-09 09:49:59 -0300

    .

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,10 @@ import { onLogin } from "@/lib/actions/userActions"
 const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      // clientId: process.env.GOOGLE_CLIENT_ID as string,
+      // clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: '273891134722-2d4pm5iqfsgevt4ihio0fl5puf6td26t.apps.googleusercontent.com',
+      clientSecret: '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',
     }),
   ],
   events: {
